136f9e684f643084f1876e724211a10af0ab0d74
RUnmesh <unmeshr.18@gmail.com>
2020-05-28 13:29:45 +0530

    Added models

---
+++ models/chatRecord.js
@@ -0,0 +1,16 @@
+const md5 = require("md5")
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define("chatRecord", {
+      sender: DataTypes.STRING,
+      message: DataTypes.STRING,
+      recipient: DataTypes.STRING
+    },{
+      getterMethods: {
+        uid: function() {return md5(this.sender + this.message + this.recipient)},
+        s_classifier: function() {return "USER"},
+        m_classifier: function() {return "MESSAGE"},
+        r_classifier: function() {return "USER"}
+      }
+    })
+  }
--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 
-async function addMessage() {
-    axios.post('http://localhost:3001/insert', {
-        sender: 'Sender',
-        message: 'Text',
-        reciver: 'Someone'
+async function addRecord(sender , message , recipient) {
+    axios.post('http://localhost:3001/insertrecord', {
+        sender: sender,
+        message: message,
+        reciver: recipient
     })
     .then((res) => {
         console.log(`statusCode: ${res.statusCode}`)
@@ -15,6 +15,36 @@ async function addMessage() {
     })
 }
 
+async function addQueue(sender , message , recipient) {
+    axios.post('http://localhost:3001/insertqueue', {
+        sender: sender,
+        message: message,
+        reciver: recipient
+    })
+    .then((res) => {
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(res)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+}
+
+async function getRecords(recipient){
+    let chat_hist = (await axios.get(`http://localhost:3001/getrecord/${recipient}`)).data
+
+    return JSON.parse(chat_hist)
+}
+
+async function getQueue(){
+    let queue = (await axios.post('http://localhost:3001/getqueue')).data
+
+    return JSON.parse(queue)
+}
+
 module.exports = {
-    addMessage: addMessage
+    addRecord: addRecord,
+    addQueue: addQueue,
+    getRecords: getRecords,
+    getQueue: getQueue
 }
