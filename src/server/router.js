5d11738c1bcde2d7bdb47c9edb6b5d8418e1b8b2
Prarabdh Garg <prarabdhgrg@gmail.com>
2020-05-29 10:23:12 +0530

    Add router to the server

---
+++ src/server/router.js
@@ -0,0 +1,40 @@
+var models  = require('../../models');
+const express = require('express');
+var router = express.Router();
+
+router.post('/insertrecord', function(req, res) {
+    models.chatRecord.create({ sender: req.body.sender, message: req.body.message,recipient:req.body.reciver }).then(function(chatrecord) {
+        console.log("Record inserted in db");
+        res.json(chatrecord);
+      }).catch((error) => {
+        console.error(error)
+    })
+});
+
+router.post('/insertqueue', function(req, res) {
+    models.messageQueue.create({ sender: req.body.sender, message: req.body.message,recipient:req.body.reciver }).then(function(messagequeue) {
+        console.log("Record inserted in queue");
+        res.json(messagequeue);
+      }).catch((error) => {
+        console.error(error)
+    })
+});
+
+router.get('/getrecord/:recip', function(req, res) {
+    models.chatRecord.findAll({ where: { recipient: req.params.recip } }).then(function(chats) {
+        res.json(chats);
+      }).catch((error) => {
+        console.error(error)
+    })
+});
+
+router.get('/getqueue', function(req, res) {
+    models.messageQueue.findAll().then(function(messageQueues) {
+        res.json(messageQueues);
+      }).catch((error) => {
+        console.error(error)
+    })
+});
+
+
+module.exports = router;
--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,14 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser")
-const router = express.Router()
-const sqlite = require('sqlite3')
 const Sequelize = require('sequelize')
 const cors = require('cors')
+const router = require('./router')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
-
-router.post('/insert', (req, res) => {
-    var sender = req.body.sender
-    var text = req.body.message
-    var reciver = req.body.reciver
-    console.log('Request = ' + sender + ' ' + text + ' ' + reciver)
-    res.end('yes')
-})
+app.use("/", router)
 
 app.listen(3001, () => {
     console.log('Database Server started at port 3001!!!')
@@ -24,19 +16,4 @@ app.listen(3001, () => {
         dialect: 'sqlite',
         storage:'/databases/messages.db'
     })
-    var Message = sequelize.define('message', {
-        sender: {
-            type: Sequelize.STRING
-        },
-        text: {
-            type: Sequelize.STRING
-        },
-        reciver: {
-            type: Sequelize.STRING
-        }
-    }, {
-        freezeTableName: true
-    })
 })
-
-app.use("/", router);
