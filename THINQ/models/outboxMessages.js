c16c40db04029e29a91adff03ddf2b770d4215c6
Prarabdh Garg <prarabdhgrg@gmail.com>
2020-06-14 13:11:40 +0530

    Update Models

--- THINQ/models/outboxMessages.js
+++ THINQ/models/outboxMessages.js
@@ -2,14 +2,16 @@
 
 module.exports = function(sequelize, DataTypes) {
     global.OutboxMessages = sequelize.define("OutboxMessages", {
-        uid: DataTypes.STRING,
+        uid: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true
+        },
         sender: DataTypes.STRING,
         action: DataTypes.INTEGER, // Currently, this would be 1 for requests and 2 for updates
         message: DataTypes.STRING,
         messageType: DataTypes.STRING,
         reciver: DataTypes.STRING,
         reciverType: DataTypes.STRING,
-        timestamp: DataTypes.DATE,
         priority: DataTypes.DOUBLE
     })
     return OutboxMessages
